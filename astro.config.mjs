5030ac872788a4fb82977412baebbdd5663e12ee
Khoirun Nawa <annawa.khoir@gmail.com>
2026-09-24 22:43:27 +0700

    fix(version): properly upgraded astro version and its dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://menyebarluaskan.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
